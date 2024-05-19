6b2e608d5fdbb7f82e948eeb99b093b95ba36d2d
arabxanrmzv <arabkhanramazanov@gmail.com>
2024-05-19 19:23:42 +0400

    add Telco Backend Managment

---
+++ telco-transport/src/main/java/com/company/telcotransport/dto/response/NodeRelationResponseDTO.java
@@ -0,0 +1,13 @@
+package com.company.telcotransport.dto.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class NodeRelationResponseDTO {
+
+    private Long parentId;
+    private Long childId;
+
+}
